6e033a706d87674238e3164bd511e5d17fa0e3cf
vahid <113128235+vahidnfc33@users.noreply.github.com>
2023-07-28 09:14:08 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1c53475c1b93a5b4928d523e5dcee5a30eae751f";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
